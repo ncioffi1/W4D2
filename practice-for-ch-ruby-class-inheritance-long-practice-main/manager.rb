488499b4dbce524c1d4c8813bd396b41d42aa734
Almoe099 <almoe099@gmail.com>
2023-11-07 14:16:53 -0500

    added bonus method in manager.rb

--- practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
@@ -1,7 +1,28 @@
+require_relative "employee.rb"
+
 class Manager < Employee
-    def initialize(name)
-        @name = name
+
+    def initialize(name, title, salary, boss)
+        super
         @employees = []
+    end
+
+    def bonus(multiplier)
+
+        total_salary = 0
+
+        if @employees == nil
+            total_salary += self.salary
+        else
+            @employees.each do |emp|
+
+                total_salary += emp.salary + emp.bonus(1)
+
+            end
+        end
+
+        bonus = total_salary * multiplier
+
     end
 
 end
