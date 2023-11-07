059d27e9d02b7d3bdf240eea45bd16a9b295293a
ncioffi1 <imnickcioffi@gmail.com>
2023-11-07 16:20:19 -0500

    commit

--- practice-for-ch-ruby-chess-long-practice-main/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -6,15 +6,31 @@ class Board
     end
 
     def [](pos)
+        x, y = pos
+        @rows[x][y]
     end
 
     def []=(pos, val)
+        x, y = pos
+        @rows[x][y] = val
     end
 
     def move_piece(color, start_pos, end_pos)
+        if valid_pos?(self[end_pos])
+            piece = self[start_pos] 
+            self[end_pos] = piece
+            self[start_pos] = nil
+        end
     end
 
     def valid_pos?(pos)
+        x, y = pos
+        if x >= 0 && x <= 7 && y >= 0 && y <= 7
+            if self[pos].empty?
+                return true
+            end
+        end
+        return false
     end
 
     def add_piece(piece, pos)
